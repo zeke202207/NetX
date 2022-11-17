72ea03e9cd9f57c8e93a3491813ab97efbcaa2ca
qizongjiang <qizongjiang@dinghantech.com>
2022-11-17 16:29:35 +0800

    优化缓存key注入方式

--- src/01_NetXFramework/01_App/ServerHost.cs
+++ src/01_NetXFramework/01_App/ServerHost.cs
@@ -51,10 +51,8 @@ public static class ServerHost
         builder.InjectUserModulesService(options.Modules, builder.Environment, builder.Configuration);
         //所有模块数据库迁移配置完毕，注入数据库迁移
         builder.Services.BuildFluentMigrator();
-        //添加日志
-        //builder.Host.UseLogging(LoggingType.Serilog);
         //Cache
-        AddCaches(builder.Services);
+        builder.Services.AddCaches();
         var app = builder.Build();
         //路由
         app.UseRouting();
@@ -70,73 +68,6 @@ public static class ServerHost
         app.UseEndpoints(endpoints => endpoints.MapControllers());
         ServiceLocator.Instance = app.Services;
         app.Run();
-    }
-
-    private static void AddCaches(IServiceCollection services)
-    {
-        var moduleInitializers = App.GetModuleInitializer();
-        List<CacheKeyDescriptor> list = new List<CacheKeyDescriptor>();
-        foreach (var item in moduleInitializers)
-        {
-            AddCacheKey(list, item.Key, item.ModuleType, item.GetType().Assembly.GetTypes());
-        }
-        var definedTypes = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(p => p.FullName.Contains(AppConst.C_APP_CACHE_ASSEMBLY_NAME))
-            ?.DefinedTypes.Select(p=>p.AsType());
-        if (null != definedTypes)
-            AddCacheKey(list, ModuleSetupConst.C_SERVERHOST_MODULE_ID, ModuleType.FrameworkModule, definedTypes.ToArray());
-        services.AddInMemoryCache(() => list);
-    }
-
-    /// <summary>
-    /// 添加fw其他模块缓存key集合
-    /// </summary>
-    /// <param name="list"></param>
-    /// <param name="moduleId"></param>
-    /// <param name="moduleType"></param>
-    /// <param name="types"></param>
-    private static void AddCacheKey(List<CacheKeyDescriptor> list, Guid moduleId, ModuleType moduleType, Type[] types)
-    {
-        var cacheKeysType = types.Where(m => m.FullName.Contains(AppConst.C_APP_CACHE_CLASS_NAME));
-        foreach (var type in cacheKeysType)
-        {
-            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
-            foreach (var field in fields)
-            {
-                var module = GetModuleOption(moduleId, moduleType);
-                if (null == field || null == module)
-                    continue;
-                var descriptor = new CacheKeyDescriptor
-                {
-                    ModuleId = module.Id.ToString(),
-                    ModuleName = module.Name,
-                    Name = field.GetRawConstantValue()?.ToString()??string.Empty,
-                    Desc = field.Name
-                };
-                var descAttr = field.GetCustomAttributes().FirstOrDefault(m =>
-                    m.GetType().IsAssignableFrom(typeof(DescriptionAttribute)));
-                if (descAttr != null)
-                    descriptor.Desc = ((DescriptionAttribute)descAttr).Description;
-                list.Add(descriptor);
-            }
-        }
-    }
-
-    /// <summary>
-    /// 获取模块设置
-    /// </summary>
-    /// <param name="moduleId"></param>
-    /// <param name="moduleType"></param>
-    /// <returns></returns>
-    private static ModuleOptions? GetModuleOption(Guid moduleId, ModuleType moduleType)
-    {
-        switch(moduleType)
-        {
-            case ModuleType.UserModule:
-                return App.GetUserModuleOptions.FirstOrDefault(p => p.Id.Equals(moduleId));
-            case ModuleType.FrameworkModule:
-            default:
-                return InternalApp.FrameworkModuleOptions;
-        }
     }
 }
 
