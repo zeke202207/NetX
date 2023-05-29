c81e7e9dd37644dc465779856fc040c55a4e7c56
qizongjiang <qizongjiang@dinghantech.com>
2023-05-29 15:57:11 +0800

    为serilog日志添加配置文件

--- src/01_NetXFramework/07_Logging/FriendlyLogging/LoggingHostBuilderExtensions.cs
+++ src/01_NetXFramework/07_Logging/FriendlyLogging/LoggingHostBuilderExtensions.cs
@@ -3,32 +3,30 @@ using Serilog;
 
 namespace NetX.FriendlyLogging;
 
+/// <summary>
+/// 添加日志
+/// </summary>
 public static class LoggingHostBuilderExtensions
 {
+    /// <summary>
+    /// 日志注入
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="logType">日志类型</param>
+    /// <returns></returns>
+    /// <exception cref="NotSupportedException"></exception>
     public static ConfigureHostBuilder UseLogging(this ConfigureHostBuilder builder, LoggingType logType = LoggingType.Serilog)
     {
         switch (logType)
         {
             case LoggingType.Serilog:
-                string OUTPUT_TEMPLATE = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} <{ThreadId}> [{Level:u3}] {Message:lj}{NewLine}{Exception}";
-                Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .Enrich.WithThreadId()
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: OUTPUT_TEMPLATE)
-                .WriteTo.File($"logs/netx-.log"
-                    , rollingInterval: RollingInterval.Day
-                    , outputTemplate: OUTPUT_TEMPLATE)
-                .CreateLogger();
-                builder.UseSerilog(Log.Logger, dispose: true);
+                builder.UseSerilog((context, services, configuration) => configuration
+                   .ReadFrom.Configuration(context.Configuration, "Serilog")
+                   .ReadFrom.Services(services));
+
                 return builder;
             default:
                 throw new NotSupportedException();
         }
     }
-}
-
-public enum LoggingType
-{
-    Serilog
 }
---
+++ src/01_NetXFramework/07_Logging/FriendlyLogging/NetxConstEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetX.FriendlyLogging
+{
+    /// <summary>
+    /// 日志类型
+    /// </summary>
+    public enum LoggingType
+    {
+        /// <summary>
+        /// Serilog提供的日志 
+        /// </summary>
+        Serilog
+    }
+}
