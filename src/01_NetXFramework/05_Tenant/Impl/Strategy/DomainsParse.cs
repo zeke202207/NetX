b190d6fc35d1ca7175b2c6685c4719c517d1bd49
qizongjiang <qizongjiang@dinghantech.com>
2023-06-19 17:20:54 +0800

    租户解析策略：域名解析、二级域名解析、请求头解析

---
+++ src/01_NetXFramework/05_Tenant/Impl/Strategy/DomainsParse.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetX.Tenants
+{
+    /// <summary>
+    /// 域名解析器
+    /// </summary>
+    public class DomainsParse : ITenantParseStrategy
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        /// <summary>
+        /// 主机解析策略
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        public DomainsParse(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// 解析租户身份 
+        /// 域名作为唯一标识
+        /// </summary>
+        /// <returns>租户身份标识</returns>
+        public async Task<string> GetTenantIdentifierAsync()
+        {
+            if (null == _httpContextAccessor.HttpContext)
+                return await Task.FromResult(string.Empty);
+            return _httpContextAccessor.HttpContext.Request.Host.Host.ToLower();
+        }
+    }
+}
