2765b6207b763a12c4f3cd6f95f8e4ea9ba6102a
qizongjiang <qizongjiang@dinghantech.com>
2023-10-09 13:04:20 +0800

    添加自服务安装功能

---
+++ src/01_NetXFramework/01_App/Options/ServiceOption.cs
@@ -0,0 +1,15 @@
+﻿using ServiceSelf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetX.App.Options;
+
+public class ServiceOption
+{
+    public string ServiceName { get; set; } = "netx";
+
+    public ServiceOptions Options { get; set; }
+}
--- src/01_NetXFramework/01_App/ServerHost.cs
+++ src/01_NetXFramework/01_App/ServerHost.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetX.App.Options;
 using NetX.Common;
 using NetX.DiagnosticLog;
 using Quartz.Util;
+using ServiceSelf;
 
 namespace NetX.App;
 
@@ -18,44 +21,51 @@ public static class ServerHost
     /// </summary>
     /// <param name="options"></param>
     /// <param name="urls"></param>
-    public static void Start(RunOption options, string urls = "")
+    public static void Start(RunOption options, string urls = "" , ServiceOption? serviceOption = null)
     {
         //获取命令行参数
         var args = Environment.GetCommandLineArgs().Skip(1).ToArray();
-        //初始化 webapplicationbuilder
-        var builder = null == options.Options ?
+        if (Service.UseServiceSelf(args, serviceOption?.ServiceName, serviceOption?.Options))
+        {
+            //初始化 webapplicationbuilder
+            var builder = null == options.Options ?
             WebApplication.CreateBuilder(args) :
             WebApplication.CreateBuilder(options.Options);
-        builder.Host.UseLogging();
-        builder.AddConfiguration(options);
-        var startUrls = urls.IsNullOrWhiteSpace() ?
-            builder.Configuration[nameof(urls)] :
-            urls;
-        if (!string.IsNullOrWhiteSpace(startUrls))
-            builder.WebHost.UseUrls(startUrls);
-        //系统logo
-        builder.Configuration.ShowNetxLogo();
-        //注入系统服务
-        builder.InjectFrameworkService(builder.Environment, builder.Configuration);
-        //注入配置服务
-        options.ActionServiceCollection?.Invoke(builder.Services, builder.Configuration);
-        //注入用户模块服务
-        builder.InjectUserModulesService(options.Modules, builder.Environment, builder.Configuration);
-        builder.InjectServiceFinally();
-        builder.Services.AddSingleton(App.GetModuleInitializer());
-        var app = builder.Build();
-        //注册系统中间件组件
-        app.InjectFrameworkApplication(builder.Environment);
-        //注册配置中间件组件
-        options.ActionConfigure?.Invoke(app);
-        //注册应用中间件组件
-        app.InjectUserModulesApplication(options.Modules, builder.Environment);
-        app.InjectApplicationFinally();
-        options.App = app;
-        InternalApp.RootServices = app.Services;
-        InternalApp.Services = builder.Services;
-        ServiceLocator.Instance = app.Services;
-        app.Run();
+            //自服务
+            builder.Host.UseServiceSelf();
+            builder.Services.AddHostedService<AppHostedService>();
+
+            builder.Host.UseLogging();
+            builder.AddConfiguration(options);
+            var startUrls = urls.IsNullOrWhiteSpace() ?
+                builder.Configuration[nameof(urls)] :
+                urls;
+            if (!string.IsNullOrWhiteSpace(startUrls))
+                builder.WebHost.UseUrls(startUrls);
+            //系统logo
+            builder.Configuration.ShowNetxLogo();
+            //注入系统服务
+            builder.InjectFrameworkService(builder.Environment, builder.Configuration);
+            //注入配置服务
+            options.ActionServiceCollection?.Invoke(builder.Services, builder.Configuration);
+            //注入用户模块服务
+            builder.InjectUserModulesService(options.Modules, builder.Environment, builder.Configuration);
+            builder.InjectServiceFinally();
+            builder.Services.AddSingleton(App.GetModuleInitializer());
+            var app = builder.Build();
+            //注册系统中间件组件
+            app.InjectFrameworkApplication(builder.Environment);
+            //注册配置中间件组件
+            options.ActionConfigure?.Invoke(app);
+            //注册应用中间件组件
+            app.InjectUserModulesApplication(options.Modules, builder.Environment);
+            app.InjectApplicationFinally();
+            options.App = app;
+            InternalApp.RootServices = app.Services;
+            InternalApp.Services = builder.Services;
+            ServiceLocator.Instance = app.Services;
+            app.Run();
+        }
     }
 
     /// <summary>
@@ -65,12 +75,14 @@ public static class ServerHost
     /// <returns></returns>
     private static void ShowNetxLogo(this ConfigurationManager config)
     {
-        if (Console.WindowWidth <= 0 || Console.WindowHeight <= 0)
-            return;
-        var fontcolor = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine
-        (@$"
+        try
+        {
+            if (Console.WindowWidth <= 0 || Console.WindowHeight <= 0)
+                return;
+            var fontcolor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine
+            (@$"
                     .-----------------. .----------------.  .----------------.  .----------------.   
                     | .--------------. || .--------------. || .--------------. || .--------------. |  
                     | | ____  _____  | || |  _________   | || |  _________   | || |  ____  ____  | |    
@@ -83,18 +95,23 @@ public static class ServerHost
                     | '--------------' || '--------------' || '--------------' || '--------------' |  
                     '----------------'  '----------------'  '----------------'  '----------------'   
         ");
-        var c = Console.GetCursorPosition();
-        string output = $"版本：{config.GetValue<string>("netxinfo:version")}";
-        Console.SetCursorPosition((Console.WindowWidth - output.Length) / 2, c.Top);
-        Console.WriteLine(output);
-        c = Console.GetCursorPosition();
-        output = $"{config.GetValue<string>("netxinfo:github")}";
-        Console.SetCursorPosition((Console.WindowWidth - output.Length) / 2, c.Top);
-        Console.WriteLine(output);
-        c = Console.GetCursorPosition();
-        Console.SetCursorPosition(0, c.Top);
-        Console.WriteLine();
-        Console.ForegroundColor = fontcolor;
+            var c = Console.GetCursorPosition();
+            string output = $"版本：{config.GetValue<string>("netxinfo:version")}";
+            Console.SetCursorPosition((Console.WindowWidth - output.Length) / 2, c.Top);
+            Console.WriteLine(output);
+            c = Console.GetCursorPosition();
+            output = $"{config.GetValue<string>("netxinfo:github")}";
+            Console.SetCursorPosition((Console.WindowWidth - output.Length) / 2, c.Top);
+            Console.WriteLine(output);
+            c = Console.GetCursorPosition();
+            Console.SetCursorPosition(0, c.Top);
+            Console.WriteLine();
+            Console.ForegroundColor = fontcolor;
+        }
+        catch 
+        {
+            //不处理由于服务等原因导致没有界面产生的异常
+        }
     }
 }
 
